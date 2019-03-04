c1487ce1e2b3bee6d0f96d01b44b4b8ecab6d560
ayushi <am2862@njit.edu>
2019-03-04 12:37:51 -0500

    Creating Home and About Pages of website using laravel

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')
+
+<h1>Welcome to my About Page - Ayushi Meitrey</h1>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')
+
+<h1>Welcome to my Home Page - Ayushi Meitrey</h1>
+
+@stop
