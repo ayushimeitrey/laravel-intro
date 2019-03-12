9c89954c3951c80084a9ae042214117870241dad
ayushi <am2862@njit.edu>
2019-03-12 10:51:02 -0400

    Feature: Content add in Home Page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,11 +3,33 @@
 @section('content')
 
     <div class="container">
-<h1 style="color:#b91d19">Welcome to my Home Page - Ayushi Meitrey</h1>
+<h1 style="color:#b91d19">Welcome to my Home Page - am2862</h1>
 <a href="https://www.tutorialspoint.com/laravel/" class="btn btn-primary" role="button">LEARN LARAVEL</a>
 <a href="https://www.w3schools.com/bootstrap4/" class="btn btn-primary" role="button">LEARN BOOTSTRAP</a>
 <a href="https://www.w3schools.com/php/" class="btn btn-primary" role="button">LEARN PHP</a>
 <a href="https://www.w3schools.com/html/default.asp" class="btn btn-primary" role="button">LEARN HTML</a>
+<br><br><br>
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </div>
+        </div>
+
+        <hr>
+
     </div>
 
 
